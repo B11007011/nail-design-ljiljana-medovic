feae460574f7ccaed8bf63186a3be059bd0fa8d1
stefanelli990 <stefan.mohenski@gmail.com>
2024-09-17 13:22:07 +0200

    fixing seo

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -9,11 +9,12 @@ export default function AboutPage() {
   return (
     <HelmetProvider>
       <Helmet>
-      <title>Nail Design Ljiljana Medović | O Nama</title>
-      <meta name="description" content="Saznajte više o nama i našoj posvećenosti nezi noktiju i lepoti."/>
-      <meta property="og:title" content="Nail Design Ljiljana Medović | O Nama" />
-      <meta property="og:description" content="Saznajte više o nama i našoj posvećenosti nezi noktiju i lepoti." />
-      <meta property="og:url" content="https://naildesign-ljiljanamedovic.com/o-nama" />
+        <title>Nail Design Ljiljana Medović | O Nama</title>
+        <meta name="description" content="Saznajte više o nama i našoj posvećenosti nezi noktiju i lepoti."/>
+        <meta property="og:image" content="https://naildesign-ljiljanamedovic.com/og-image-about.webp" />
+        <meta property="og:title" content="Nail Design Ljiljana Medović | O Nama" />
+        <meta property="og:description" content="Saznajte više o nama i našoj posvećenosti nezi noktiju i lepoti." />
+        <meta property="og:url" content="https://naildesign-ljiljanamedovic.com/o-nama" />
       </Helmet>
       <Navbar/>
       <BreadCrumb page="O nama" />
