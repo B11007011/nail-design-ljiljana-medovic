97fc07374bc7945e559b04e78ea154a583bfd8c4
stefanelli990 <stefan.mohenski@gmail.com>
2024-10-18 15:39:18 +0200

    fixed benefits layout

--- src/sections/Benefits.jsx
+++ src/sections/Benefits.jsx
@@ -1,11 +1,10 @@
 import { counter } from "../data"
 
 export default function Benefits() {
-
   return (
     <section>
-        <div className="wrapper bg-accent p-8 flex lg:p-14">
-            <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="wrapper ">
+            <ul className="bg-accent p-8 lg:p-14 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {counter.map((item, index) => (
                 <li className="space-y-4" key={index}>
                     <h2>{item.counterNumber}<span className="font-sans font-light text-4xl">+</span></h2>
