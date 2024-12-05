be4d62882fa6ffe53f0ee94946011c9bc54b7d1a
stefanelli990 <stefan.mohenski@gmail.com>
2024-12-05 02:53:21 +0100

    fixing benefits

--- src/sections/Benefits.jsx
+++ src/sections/Benefits.jsx
@@ -2,11 +2,11 @@ import { counter } from "../data"
 
 export default function Benefits() {
   return (
-    <section>
+    <section className="my-[50px] lg:my-[100px]">
         <div className="wrapper ">
-            <ul className="bg-accent px-4 py-[50px] flex flex-wrap justify-around gap-4">
+            <ul className="grid sm:grid-cols-2 md:grid-cols-3 gap-y-8 gap-x-16">
             {counter.map((item, index) => (
-                <li className="max-w-[310px] text-center" key={index}>
+                <li className="" key={index}>
                     <h2 className="mb-2">{item.counterNumber}<span className="font-sans font-light text-3xl lg:text-4xl">+</span></h2>
                     <h3 className="mb-4">{item.counterTitle}</h3>
                     <p>{item.counterDesc}</p>
